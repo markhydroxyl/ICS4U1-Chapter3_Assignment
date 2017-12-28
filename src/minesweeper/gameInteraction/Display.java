2510f78e2c469a51d1d857771628f0c9c9b3c19a
Felix Yu <felixyu2001@gmail.com>
2017-12-27 21:49:34 -0500

    Added MinesweeperConstants, updated Display to reflect accordingly.

--- src/minesweeper/gameInteraction/Display.java
+++ src/minesweeper/gameInteraction/Display.java
@@ -4,18 +4,12 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import minesweeper.tiles.*;
+import util.MinesweeperConstants;
 
 public class Display {
-	int SCREEN_WIDTH;
-	int SCREEN_HEIGHT;
-	int X_OFFSET;
-	int Y_OFFSET;
 	
 	public Display() {
-		SCREEN_WIDTH = 500;
-		SCREEN_HEIGHT = 500;
-		X_OFFSET = 50;
-		Y_OFFSET = 50;
+		
 	}
 	
 	public void display(Tile[][] aArray, Pane aRoot) {
@@ -24,35 +18,35 @@ public class Display {
 		for (int row=0; row<aArray.length; row++) {
 			for (int col=0; col<aArray[0].length; col++) {
 				Canvas tileCanvas = new Canvas();
-				aArray[row][col].displayTile(tileCanvas, row, col, aRoot, X_OFFSET, Y_OFFSET);
+				aArray[row][col].displayTile(tileCanvas, row, col, aRoot, MinesweeperConstants.X_OFFSET, MinesweeperConstants.Y_OFFSET);
 			}
 		}
 		
-		drawLines(aRoot, aArray[0][0].getHeight(), aArray[0][0].getWidth(), aArray.length, aArray[0].length);
+		drawLines(aRoot, aArray.length, aArray[0].length);
 		
 		//Print game to console for reference
 		printToConsole(aArray);
 	}
 	
-	private void drawLines(Pane aRoot, int tileHeight, int tileWidth, int numRows, int numCols) {
+	private void drawLines(Pane aRoot, int numRows, int numCols) {
 		
 		for (int i=0; i<=numCols; i++) {
-			Canvas vertCanvas = new Canvas(2, numCols*tileHeight);
+			Canvas vertCanvas = new Canvas(2, numCols*MinesweeperConstants.TILE_HEIGHT);
 			GraphicsContext vertGC = vertCanvas.getGraphicsContext2D();
 			vertGC.setLineWidth(2);
-			vertGC.strokeLine(0, 0, 0, numCols*tileHeight);
-			vertCanvas.setTranslateX(i*tileWidth+X_OFFSET);
-			vertCanvas.setTranslateY(Y_OFFSET);
+			vertGC.strokeLine(0, 0, 0, numCols*MinesweeperConstants.TILE_HEIGHT);
+			vertCanvas.setTranslateX(i*MinesweeperConstants.TILE_WIDTH+MinesweeperConstants.X_OFFSET);
+			vertCanvas.setTranslateY(MinesweeperConstants.Y_OFFSET);
 			aRoot.getChildren().add(vertCanvas);
 		}
 		
 		for (int i=0; i<=numRows; i++) {
-			Canvas horCanvas = new Canvas(numRows*tileWidth, 2);
+			Canvas horCanvas = new Canvas(numRows*MinesweeperConstants.TILE_WIDTH, 2);
 			GraphicsContext horGC = horCanvas.getGraphicsContext2D();
 			horGC.setLineWidth(2);
-			horGC.strokeLine(0, 0, numRows*tileWidth, 0);
-			horCanvas.setTranslateX(X_OFFSET);
-			horCanvas.setTranslateY(i*tileHeight+Y_OFFSET);
+			horGC.strokeLine(0, 0, numRows*MinesweeperConstants.TILE_WIDTH, 0);
+			horCanvas.setTranslateX(MinesweeperConstants.X_OFFSET);
+			horCanvas.setTranslateY(i*MinesweeperConstants.TILE_WIDTH+MinesweeperConstants.Y_OFFSET);
 			aRoot.getChildren().add(horCanvas);
 		}
 	}
---
+++ src/util/MinesweeperConstants.java
@@ -0,0 +1,32 @@
+package util;
+
+public class MinesweeperConstants {
+	public final static int TILE_HEIGHT = 40;
+	public final static int TILE_WIDTH = 40;
+	public final static int X_OFFSET = 50;
+	public final static int Y_OFFSET = 50;
+	
+	public final static int EASY_NUM_ROWS = 10;
+	public final static int EASY_NUM_COLS = 10;
+	public final static int EASY_SCREEN_WIDTH = 2*X_OFFSET+EASY_NUM_ROWS;
+	public final static int EASY_SCREEN_HEIGHT = 2*Y_OFFSET+EASY_NUM_COLS;
+	public final static double EASY_MINE_DENSITY = 0.1;
+	
+	public final static int MED_NUM_ROWS = 20;
+	public final static int MED_NUM_COLS = 20;
+	public final static int MED_SCREEN_WIDTH = 2*X_OFFSET+MED_NUM_ROWS;
+	public final static int MED_SCREEN_HEIGHT = 2*Y_OFFSET+MED_NUM_COLS;
+	public final static double MED_MINE_DENSITY = 0.15;
+	
+	public final static int HARD_NUM_ROWS = 30;
+	public final static int HARD_NUM_COLS  = 30;
+	public final static int HARD_SCREEN_WIDTH = 2*X_OFFSET+HARD_NUM_ROWS;
+	public final static int HARD_SCREEN_HEIGHT = 2*Y_OFFSET+HARD_NUM_COLS;
+	public final static double HARD_MINE_DENSITY = 0.15;
+	
+	public final static int LUNA_NUM_ROWS = 40;
+	public final static int LUNA_NUM_COLS = 40;
+	public final static int LUNA_SCREEN_WIDTH = 2*X_OFFSET+LUNA_NUM_ROWS;;
+	public final static int LUNA_SCREEN_HEIGHT = 2*Y_OFFSET+LUNA_NUM_COLS;
+	public final static double LUNA_MINE_DENSITY = 0.4;
+}
