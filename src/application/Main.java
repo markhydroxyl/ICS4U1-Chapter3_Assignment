27fe2ac55e806aa6a72cd9e3f0d0902134f57e6c
Ryanturnbull <Ryanturnbull@ryans-mbp-7.crescentschool.local>
2018-01-24 15:48:13 -0500

    Test Commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,12 +15,13 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch(Exception e) { 
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+		System.out.print("Ryan Test");
 	}
 }
